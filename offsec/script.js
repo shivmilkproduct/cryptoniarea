ea73e95b65918b82b65b59a1bbb4625c5fbe0e53
Shiv Milk Product <ajaxblackhat@gmail.com>
2026-05-16 15:59:34 +0530

    Add files via upload

---
+++ offsec/script.js
@@ -0,0 +1,53 @@
+document.getElementById("registrationForm").addEventListener("submit", async function(e) {
+
+    e.preventDefault();
+
+    const data = {
+
+        email: document.getElementById("email").value,
+
+        fullName: document.getElementById("fullName").value,
+
+        mobile: document.getElementById("mobile").value,
+
+        college: document.getElementById("college").value,
+
+        level: document.getElementById("level").value,
+
+        previousSession: document.getElementById("previousSession").value,
+
+        commitment: document.getElementById("commitment").checked
+
+    };
+
+    try {
+
+        const response = await fetch(
+            "https://script.google.com/macros/s/AKfycbx4sw46Pd0D3G2bP5vyWOUY_jWvVDxRbE2pIXEeQSc4ECr_cuRyc_u_k_O8Np1rbXyT/exec",
+            {
+                method: "POST",
+                body: JSON.stringify(data)
+            }
+        );
+
+        if (response.ok) {
+
+            alert("Registration Successful!");
+
+            document.getElementById("registrationForm").reset();
+
+        } else {
+
+            alert("Something went wrong.");
+
+        }
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Error submitting form.");
+
+    }
+
+});
